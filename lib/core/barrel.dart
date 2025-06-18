459ee33c486168214f212d169c0d91c8b98e643a
Abdullah-Sherif <abdullahsherif73+git@gmail.com>
2025-06-18 16:10:34 +0300

    Add CustomColors

--- lib/core/barrel.dart
+++ lib/core/barrel.dart
@@ -4,4 +4,5 @@ export 'env.dart';
 export 'extentions/barrel.dart';
 export 'presentation/barrel.dart';
 export 'router/barrel.dart';
+export 'style/barrel.dart';
 export 'util/barrel.dart';
---
+++ lib/core/style/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color mainPurple = Color(0xFF8900FE);
+  static const Color linkBlue = Color(0xFF1877F2);
+  static const Color lightGray = Color.fromRGBO(196, 196, 196, 0.2);
+  static const Color black = Color.fromRGBO(0, 0, 0, 1);
+}
