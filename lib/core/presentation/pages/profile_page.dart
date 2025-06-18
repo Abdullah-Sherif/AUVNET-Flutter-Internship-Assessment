2f113aa6d17ca54475dfa12f1a48cb8f2a6cd92d
Abdullah-Sherif <abdullahsherif73+git@gmail.com>
2025-06-19 00:10:44 +0300

    Added a SignOut button for testing

--- lib/core/presentation/pages/profile_page.dart
+++ lib/core/presentation/pages/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auvnet_internship_assessment/features/auth/presentation/states/auth_state/auth_bloc.dart';
 
 @RoutePage()
 class ProfilePage extends StatelessWidget {
@@ -7,6 +9,15 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Profile Page')));
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            context.read<AuthBloc>().add(const SignOutRequested());
+          },
+          child: const Text('Sign Out'),
+        ),
+      ),
+    );
   }
 }
